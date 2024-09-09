d7187cfdb7e2440140b293bca99dab2b4eb89c9d
Dave Ogle <dogle@scottlogic.com>
2024-09-09 11:46:58 +0100

    add custom types

--- types.hs
+++ types.hs
@@ -1,4 +1,20 @@
 addThree :: Int -> Int -> Int -> Int -- Type declaration
 addThree x y z = x + y + z
+
+-- custom type
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
+
+-- record type
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavour :: String
+                     } deriving (Show) 
+
 main = do
+    let guy = Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
     print (addThree 1 2 3)
+    print (firstName guy)
+    print (Circle 10 20 5)
